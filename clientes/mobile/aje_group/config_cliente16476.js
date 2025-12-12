4210bb5da1858f84ac5fd1d04d18cc087ea885fa
orzaca <140673379+orzaca@users.noreply.github.com>
2025-12-12 15:46:46 -0500

    Add files via upload

---
+++ clientes/mobile/aje_group/config_cliente16476.js
@@ -0,0 +1,12 @@
+/* config_cliente16476.js
+   Configuración exclusiva para Centro Empresarial San Fernando Plaza (16476)
+   ADVERTENCIA: si pones username/password aquí, cualquiera que vea la fuente los podrá leer.
+*/
+
+const CONFIG_CLIENTE = {
+  apiUrl: "https://visionlatam.securitas.com/rest/v1/",
+  username: "CO0819955",
+  password: "Colombia.01",
+  clienteId: "16476",
+  clienteName: "Centro Empresarial San Fernando Plaza"
+};
---
+++ clientes/mobile/carnicos/config_carnicos.js
@@ -0,0 +1,9 @@
+/* config_carnicos.js - */
+
+const CONFIG_CLIENTE = {
+  apiUrl: "https://visionlatam.securitas.com/rest/v1/",
+  username: "CO0819955",
+  password: "Colombia.01",
+  clienteId: "14082",
+  clienteName: "Carnicos Barranquilla"
+};
